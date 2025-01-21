4b5b660eb9c02b7912d9b4a641079a00414a48bc
Mathis Boulais <mathis.boulais@naxos.fr>
2025-01-21 13:57:46 +0100

    init

---
+++ AdventOfCode1/AdventOfCode1/Program.cs
@@ -0,0 +1,77 @@
+﻿/*using System;
+using System.IO;
+using System.Linq;
+
+class Day1
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("C:\\Users\\mboulais\\Documents\\perso\\AdventOfCode\\input.txt");
+
+        int[][] lists = lines
+            .Select(line => line.Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToArray())
+            .ToArray();
+
+        int[] leftList = lists.Select(pair => pair[0]).ToArray();
+        int[] rightList = lists.Select(pair => pair[1]).ToArray();
+
+        Array.Sort(leftList);
+        Array.Sort(rightList);
+
+        int totalDistance = 0;
+        for (int i = 0; i < leftList.Length; i++)
+        {
+            totalDistance += Math.Abs(leftList[i] - rightList[i]);
+        }
+
+        Console.WriteLine($"Total Distance: {totalDistance}");
+    }
+}*/
+
+//PART 2
+
+using System;
+using System.IO;
+using System.Linq;
+
+class Day1
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("C:\\Users\\mboulais\\Documents\\perso\\AdventOfCode\\input.txt");
+
+        List<int> leftList = new List<int>();
+        List<int> rightList = new List<int>();
+
+        foreach (string line in lines)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                leftList.Add(int.Parse(parts[0]));
+                rightList.Add(int.Parse(parts[1]));
+            }
+        }
+
+        Dictionary<int, int> rightListCounts = new Dictionary<int, int>();
+        foreach(int number in rightList)
+        {
+            if (!rightListCounts.ContainsKey(number))
+            {
+                rightListCounts[number] = 0;
+            }
+            rightListCounts[number]++;
+        }
+
+        int similarityScore = 0;
+        foreach(int number in leftList)
+        {
+            if(rightListCounts.ContainsKey(number))
+            {
+                similarityScore += number * rightListCounts[number];
+            }
+        }
+
+        Console.WriteLine(similarityScore);
+    }
+}
---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Day2
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("C:\\Users\\mboulais\\Documents\\perso\\AdventOfCode\\input.txt");
+
+        int safeCount = 0;
+
+        foreach (string line in lines)
+        {
+            if (!string.IsNullOrWhiteSpace(line)) {
+
+                int[] levels = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            if (IsSafeReport(levels))
+            {
+                safeCount++;
+            }
+        }
+       }
+        Console.WriteLine(safeCount);
+    }
+
+    static bool IsSafeReport(int[] levels)
+    {
+        bool isincreasing = true;
+        bool isDecreasing = true;
+
+        for(int i = 1; i < levels.Length; i++) { 
+            int difference = levels[i] - levels[i -1];
+
+            if(Math.Abs(difference) < 1 ||  Math.Abs(difference) > 3)
+            {
+                return false;
+            }
+
+            if(difference > 0)
+            {
+                isDecreasing = false;
+            }
+            else if(difference < 0)
+            {
+                isincreasing = false;
+            }
+        }
+
+        return isincreasing || isDecreasing;
+    }
+
+}
+

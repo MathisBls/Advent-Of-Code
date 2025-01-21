f886ad9caae3be546cec5f2c48fdb8fbbe2a8702
Mathis Boulais <mathis.boulais@naxos.fr>
2025-01-21 15:52:41 +0100

    day2

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -50,6 +50,81 @@ class Day2
         }
 
         return isincreasing || isDecreasing;
+    }
+
+}
+
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Day2
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("C:\\Users\\mboulais\\Documents\\perso\\AdventOfCode\\input.txt");
+
+        int safeCount = 0;
+
+        foreach (string line in lines)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                int[] levels = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                if (IsSafeReport(levels) || CanBecomeSafeWithDampener(levels))
+                {
+                    safeCount++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Number of safe reports: {safeCount}");
+    }
+
+    static bool IsSafeReport(int[] levels)
+    {
+        bool isincreasing = true;
+        bool isDecreasing = true;
+
+        for (int i = 1; i < levels.Length; i++)
+        {
+            int difference = levels[i] - levels[i - 1];
+
+            if (Math.Abs(difference) < 1 || Math.Abs(difference) > 3)
+            {
+                return false;
+            }
+
+            if (difference > 0)
+            {
+                isDecreasing = false;
+            }
+            else if (difference < 0)
+            {
+                isincreasing = false;
+            }
+        }
+
+        return isincreasing || isDecreasing;
+    }
+
+    static bool CanBecomeSafeWithDampener(int[] levels)
+    {
+        for(int i = 0; i < levels.Length; i++)
+        {
+            int[] modifedLevels = levels.Where((_, index) => index != i).ToArray();
+
+            if (IsSafeReport(modifedLevels))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
